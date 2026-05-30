066a671c455a9ac00415fb3234203f3d2bc6392e
KARTAVYA-KUMAR <148651939+KARTAVYA-KUMAR@users.noreply.github.com>
2026-05-30 16:30:07 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://evantic-1.onrender.com',
+    baseURL: 'https://evantic-1.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
